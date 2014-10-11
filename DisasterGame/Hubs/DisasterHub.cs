f5cc2eac4d6930da096adbbc8efb01593185f0d0
Jo Osborne <joanne.osborne@gmail.com>
2014-10-11 11:23:30 +0100

    initial commit

---
+++ DisasterGame/Hubs/DisasterHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace DisasterGame.Hubs
+{
+    public class DisasterHub : Hub
+    {
+        //public void Send(string name, string message)
+        //{
+        //    Clients.All.broadcastMessage(name, message);
+        //}
+    }
+}
